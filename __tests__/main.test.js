9bc6c7815ac8e0f6481c004498b1e7aa32692317
poltos <poltos.pro@gmail.com>
2020-09-10 22:48:16 +0300

    fix test

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,22 +1,17 @@
 import fs from 'fs';
 import path from 'path';
-import url from 'url';
-// import { TestScheduler } from 'jest';
 import gendiff from '../src/gendiff.js';
 
-// const __filename = url.fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(path.__filename);
 
-const getFixturePath = (filename) => path.join(process.cwd(), '..', 'frontend-project-lvl2/__fixtures__', filename);
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-
 test.each([
-  ['json', 'json', 'output.json.txt'],
-  ['json', 'output.stylish.txt'],
-  ['json', 'json', 'output.plain.txt'],
-  ['yaml', 'output.stylish.txt'],
-  ['ini', 'output.stylish.txt'],
+  ['json', 'output-json.json', 'json'],
+  ['json', 'output-stylish.txt'],
+  ['json', 'output-plain.txt', 'plain'],
+  ['yaml', 'output-stylish.txt'],
+  ['ini', 'output-stylish.txt'],
 ])('gendiff(%s, %s)', (isFile, expectedFile, format) => {
   const firstFile = getFixturePath(`filepath1.${isFile}`);
   const secondFile = getFixturePath(`filepath2.${isFile}`);
