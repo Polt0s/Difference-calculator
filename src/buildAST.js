c752bdd64ff9cd8d5323c5f853e6420ef478be7e
poltos <poltos.pro@gmail.com>
2020-09-16 23:25:58 +0300

    fix tree

--- src/buildAST.js
+++ src/buildAST.js
@@ -1,48 +1,45 @@
 import _ from 'lodash';
 
+const indent = (depth, tab = '    ') => tab.repeat(depth);
+
 const convertToString = (value, data) => {
   if (!_.isObject(value)) {
     return value;
   }
-  const newKeys = Object.keys(value);
-  const searchKeys = newKeys.map((key) => `          ${key}: ${value[key]}`);
+  const newKeys = Object.keys(value)
+  const searchKeys = newKeys.map((key) => `    ${key}: ${value[key]}`);
   if (_.isObject(value)) {
-    return `{\n${data}${searchKeys}\n${data}}`;
+    const output = ['{', searchKeys, '}'];
+    return output.join(`\n${indent(data)}`);
   }
   return `wrong format - ${value}`;
 };
-const indent = (depth, tab = '  ') => tab.repeat(depth);
 
-const getNewTree = (ast) => {
-  const iter = (ast, depth) => {
-    const statusKeys = ast
-      .map((tree) => {
-        const {
-          key, type, value, oldValue, newValue, children,
-        } = tree;
-        switch (type) {
-          case 'added':
-            return `${indent(depth)}    + ${key}: ${convertToString(value, depth + 1)}`;
-          case 'delete':
-            return `${indent(depth)}    - ${key}: ${convertToString(value, depth + 1)}`;
-          case 'unchanged':
-            return `${indent(depth)}      ${key}: ${convertToString(value, depth + 1)}`;
-          case 'changed':
-            return `${indent(depth)}    - ${key}: ${convertToString(newValue, depth + 1)}\n${indent(depth)}`
-              + `${indent(depth)}    + ${key}: ${convertToString(oldValue, depth + 1)}\n${indent(depth)}`;
-          case 'nested':
-            return `${indent(depth)}      ${key}: ${iter(children, depth + 2)}`;
-          default:
-            return `wrong data type - ${type}`;
-        }
-      })
-      .flat();
-    return `{  \n${statusKeys.join('\n')}  \n${indent(depth)}}`;
-    // const output = statusKeys.join('\n');
-    // return `{\n${output}\n}`;
-  }
-  return iter(ast, 0)
-
+const getNewTree = (tree, depth = 0) => {
+  const statusKeys = tree
+    .map((tree) => {
+      const {
+        key, type, value, oldValue, newValue, children,
+      } = tree;
+      switch (type) {
+        case 'added':
+          return `  + ${key}: ${convertToString(value, depth + 1)}`;
+        case 'delete':
+          return `  - ${key}: ${convertToString(value, depth + 1)}`;
+        case 'unchanged':
+          return `    ${key}: ${convertToString(value, depth + 1)}`;
+        case 'changed':
+          return `  - ${key}: ${convertToString(newValue, depth + 1)}\n${indent(depth)}`
+            + `  + ${key}: ${convertToString(oldValue, depth + 1)}`;
+        case 'nested':
+          return `    ${key}: ${getNewTree(children, depth + 1)}`;
+        default:
+          return `wrong data type - ${type}`;
+      }
+    })
+    .flat();
+  const output = ['{', ...statusKeys, '}'];
+  return output.join(`\n${indent(depth)}`);
 };
 
 export default getNewTree;
